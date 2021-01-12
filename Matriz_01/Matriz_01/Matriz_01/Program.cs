e1dca66e90285c90979d2c8ced3ab8cdd51500a5
Julio Nascimento <juliodive@gmail.com>
2021-01-12 10:08:51 -0400

    first matrix

---
+++ Matriz_01/Matriz_01/Matriz_01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Matriz_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < N; j++)
+                {
+                    matrix[i, j] = int.Parse(valores[j]);
+                }
+            }
+            Console.WriteLine("DIAGONAL PRINCIPAL:");
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(matrix[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Total de numeros negativos: " + count);
+        }
+    }
+}
+
+
+
+
+
