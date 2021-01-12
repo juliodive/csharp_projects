839778ec40675e008e62d54af5a7d208f34c86d8
Julio Nascimento <juliodive@gmail.com>
2021-01-12 15:56:09 -0400

    atualizacao

--- Matriz_06/Matriz_06/Program.cs
+++ Matriz_06/Matriz_06/Program.cs
@@ -6,7 +6,40 @@ namespace Matriz_06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int M, N;
+            // Entrada de dados da matriz
+            string[] entrada = Console.ReadLine().Split(' ');
+            M = int.Parse(entrada[0]);
+            N = int.Parse(entrada[1]);
+            // Fim
+
+            int[,] matrix = new int[M, N];
+
+            // Entrada de valores
+            for (int i = 0; i < M; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    matrix[i, j] = int.Parse(valores[j]);
+                }
+            }
+            //Fim
+
+
+            for (int i = 0; i < M; i++)
+            {
+                int maior = matrix[0, 0];
+                for (int j = 0; j < N; j++)
+                {
+                    if (matrix[i, j] > maior)
+                    {
+                        maior = matrix[i, j];
+                    }
+                }
+
+                Console.WriteLine(maior);
+            }
         }
     }
 }
---
+++ Matriz_07/Matriz_07/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Matriz_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada de dados da matriz
+            int N = int.Parse(Console.ReadLine());
+            //Fim
+            int[,] matrix = new int[N, N];
+            //Entrada de valores
+            for (int i = 0; i < N; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    matrix[i, j] = int.Parse(valores[j]);
+                }
+            }
+            //FIM
+            int soma = 0;
+            // Leitura de dados e Saida
+            for (int i = 0; i < N; i++)
+            {
+                soma += matrix[i, i];
+            }
+            Console.WriteLine(soma);
+            //FIM
+        }
+    }
+}
