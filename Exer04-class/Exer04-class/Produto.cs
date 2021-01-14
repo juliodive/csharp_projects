e4a3c8c2d7b9df9c9915d4eb7cf4ee11a012065e
Julio Nascimento <juliodive@gmail.com>
2021-01-14 16:23:29 -0400

    Exercicio Class Attributes Methods

---
+++ Exer04-class/Exer04-class/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization; 
+
+namespace Exer04_class
+{
+    class Produto   
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+
+        public void AdicionarProdutos (int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + " $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + " , "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exer04-class/Exer04-class/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exer04_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            Console.WriteLine("Entre os dados do Produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos  a ser adicionados ao estoque: ");
+            int qto = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qto);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos  a ser removido ao estoque: ");
+            qto = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qto);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+        }
+    }
+}
