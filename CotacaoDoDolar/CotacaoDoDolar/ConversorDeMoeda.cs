86e733dbcf0c5f6ad14b25e3cfa27f578c645b1c
Julio Nascimento <juliodive@gmail.com>
2021-01-20 17:56:11 -0400

    Converso de Moeda

---
+++ CotacaoDoDolar/CotacaoDoDolar/ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace CotacaoDoDolar {
+    class ConversorDeMoeda {
+        public static double Iof = 6.0;
+        public static double Cotacao(double dolar, double quantia) {
+            double result = dolar * quantia;
+            return result + result * Iof / 100; 
+        }
+
+    }
+}
---
+++ CotacaoDoDolar/CotacaoDoDolar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace CotacaoDoDolar {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cot_dol = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quant_dol = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double total = ConversorDeMoeda.Cotacao(quant_dol, cot_dol);
+            Console.WriteLine(total.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
