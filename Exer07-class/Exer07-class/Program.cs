cd479d49ce8dee01a31dd2ea9adf9ef79ea3d174
Julio Nascimento <juliodive@gmail.com>
2021-01-20 17:24:32 -0400

    Exercicio 07

---
+++ Exer07-class/Exer07-class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exer07_class {
+    class Program {
+        static void Main(string[] args) {
+            Aluno al = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            al.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            al.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            al.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            al.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = " + al.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            if (al.Aprovado()) {
+                Console.WriteLine("Aprovado");
+            }
+            else {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Faltaram " + al.NotaRestante().ToString("F2", CultureInfo.InvariantCulture) + "Pontos");
+            }
+            
+        }
+    }
+}
